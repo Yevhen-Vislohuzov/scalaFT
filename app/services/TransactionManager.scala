02b9c009220f24409de5172d8dbf8f72391c8335
yevhen <yevhenvislohuzov@gmail.com>
2018-01-22 23:26:58 +0200

    remove unused classes

--- app/services/TransactionManager.scala
+++
@@ -1,9 +0,0 @@
-package services
-
-import scala.collection.mutable
-
-trait TransactionManager {
-  def getTransactions: mutable.MutableList[Transaction]
-  def addTransaction(trs: Transaction): Unit
-
-}
